1a1ff6a96fded13f2960ea3e0e1f36ac2c977da7
AlexMarFlo <ing.amarflo@gmail.com>
2018-08-13 14:19:04 -0500

    Adicion de la clase CheckConnection

---
+++ marflolibrary/src/main/java/mx/com/marflo/marflolibrary/CheckConnection.java
@@ -0,0 +1,46 @@
+package mx.com.marflo.marflolibrary;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * @version 1
+ * @author Ing Alejandro Martínez Flores
+ * @since 13/08/2018
+ */
+public class CheckConnection {
+
+    /**
+     * Método con el que se valida que el dispositivo cuente con conexión a internet para ejecutar las consultas al servidor
+     * @param context   Contexto que llama al método
+     * @return          Valor boleano con la validación
+     */
+    public static boolean conexionEstablecida(Context context){
+        return conectadoPorDatos(context) || conectadoPorWiFi(context);
+    }
+    private static boolean conectadoPorDatos(Context context){
+        ConnectivityManager connectivity = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivity != null){
+            NetworkInfo info = connectivity.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+            if (info != null){
+                return info.isConnected();
+            }
+        }
+
+        return false;
+    }
+    private static boolean conectadoPorWiFi(Context context){
+        ConnectivityManager connectivity = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        if (connectivity != null){
+            NetworkInfo info = connectivity.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+            if (info != null){
+                return info.isConnected();
+            }
+        }
+
+        return false;
+    }
+}

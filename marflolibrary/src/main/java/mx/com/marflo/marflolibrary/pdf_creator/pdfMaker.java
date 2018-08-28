f8478f46444fe036a10a09a20f95b4b30129ca16
AlexMarFlo <ing.amarflo@gmail.com>
2018-08-28 15:45:41 -0500

    Actualización de pdfMaker

--- marflolibrary/src/main/java/mx/com/marflo/marflolibrary/pdf_creator/pdfMaker.java
+++ marflolibrary/src/main/java/mx/com/marflo/marflolibrary/pdf_creator/pdfMaker.java
@@ -1,11 +1,10 @@
 package mx.com.marflo.marflolibrary.pdf_creator;
 
-import android.annotation.SuppressLint;
-import android.support.v7.app.AlertDialog;
-import android.view.LayoutInflater;
-import android.view.View;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 
-import mx.com.marflo.marflolibrary.PersonalDialog;
+import java.io.File;
+
 import mx.com.marflo.marflolibrary.R;
 
 /**
@@ -16,45 +15,41 @@ import mx.com.marflo.marflolibrary.R;
 public class pdfMaker {
     private pdfCreator pdfCreator;
     private makeFile task;
+    private ProgressDialog pd;
 
-    public pdfMaker(pdfCreator pdfCreator, pdfCreatorCallback callback){
+    public pdfMaker(pdfCreator pdfCreator, final pdfCreatorCallback callback){
         this.pdfCreator = pdfCreator;
-        task = new makeFile(pdfCreator, callback);
-    }
+        task = new makeFile(pdfCreator, new pdfCreatorCallback() {
+            @Override
+            public void onFinish(File f) {
+                pd.dismiss();
+                callback.onFinish(f);
+            }
 
-    public void run(){
+            @Override
+            public void onException(Exception e) {
+                pd.dismiss();
+                callback.onException(e);
+            }
+        });
+
         initDialog();
         task.execute();
     }
 
     private void initDialog(){
-        new PersonalDialog()
-                .setYesTitle(pdfCreator.getContext().getResources().getString(R.string.CANCELAR))
-                .setView(getView())
-                .showDialog(pdfCreator.getContext(),
-                        pdfCreator.getContext().getResources().getString(R.string.pdf_maker_creating_file),
-                        null,null, new PersonalDialog.Callback() {
-                    @Override
-                    public void onPositiveClick(AlertDialog alertDialog) {
-                        alertDialog.dismiss();
-                        task.cancel(true);
-                    }
-
-                    @Override
-                    public void onNeutralClick(AlertDialog alertDialog) {
-
-                    }
-
-                    @Override
-                    public void onNegativeClick(AlertDialog alertDialog) {
-
-                    }
-                });
-    }
-
-    @SuppressLint("InflateParams")
-    private View getView(){
-        return LayoutInflater.from(pdfCreator.getContext()).inflate(R.layout.pdf_creator_progress,
-                null, false);
+        pd = new ProgressDialog(pdfCreator.getContext());
+        pd.setMessage(pdfCreator.getContext().getResources().getString(R.string.pdf_maker_creating_file));
+        pd.setTitle(pdfCreator.getContext().getResources().getString(R.string.pdf_maker_progres_message));
+        pd.setCancelable(true);
+        pd.setIndeterminate(true);
+        pd.show();
+        pd.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                dialog.dismiss();
+                task.cancel(true);
+            }
+        });
     }
 }

c8e66df9fb752c2dd83532a390f065016391d979
AlexMarFlo <ing.amarflo@gmail.com>
2018-09-17 10:11:10 -0500

    Adición de métodos en FilesUtils

--- marflolibrary/src/main/java/mx/com/marflo/marflolibrary/FilesUtils.java
+++ marflolibrary/src/main/java/mx/com/marflo/marflolibrary/FilesUtils.java
@@ -2,10 +2,16 @@ package mx.com.marflo.marflolibrary;
 
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
 import android.support.v4.content.FileProvider;
 import android.webkit.MimeTypeMap;
 
@@ -92,5 +98,122 @@ public class FilesUtils {
         String mimeExtension = MimeTypeMap.getFileExtensionFromUrl(extension);
 
         return MimeTypeMap.getSingleton().getMimeTypeFromExtension(mimeExtension);
+    }
+
+    /**
+     * @param context   Contexto que llama al método
+     * @param uri       Uri que contiene el archivo
+     * @return          Nombre del archivo
+     */
+    public static String getFileNameByUri(Context context, Uri uri){
+        String fileName="unknown";//default fileName
+
+        try (Cursor cursor = context.getContentResolver().query(uri, null, null, null, null, null)) {
+            if (cursor != null && cursor.moveToFirst()) {
+                fileName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+            }
+        }
+        return fileName;
+    }
+
+    /**
+     * @param context   Contexto que llama al método
+     * @param uri       Uri que contiene el archivo
+     * @return          Path del archivo
+     */
+    public static String getPath(Context context, Uri uri) {
+        String selection = null;
+        String[] selectionArgs = null;
+        // Uri is different in versions after KITKAT (Android 4.4), we need to
+        // deal with different Uris.
+        if (DocumentsContract.isDocumentUri(context.getApplicationContext(), uri)) {
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                return Environment.getExternalStorageDirectory() + "/" + split[1];
+            } else if (isDownloadsDocument(uri)) {
+                final String id = DocumentsContract.getDocumentId(uri);
+                uri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+            } else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                switch (type) {
+                    case "image":
+                        uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                        break;
+                    case "video":
+                        uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                        break;
+                    case "audio":
+                        uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                        break;
+                }
+                selection = "_id=?";
+                selectionArgs = new String[]{ split[1] };
+            }
+        }
+        if ("content".equalsIgnoreCase(uri.getScheme())) {
+            String[] projection = { MediaStore.Images.Media.DATA };
+            Cursor cursor ;
+            try {
+                cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+                if (cursor != null) {
+                    int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                    if (cursor.moveToFirst()) {
+                        return cursor.getString(column_index);
+                    }
+                    cursor.close();
+                }
+
+            } catch (Exception ignored) {
+            }
+        } else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+        return null;
+    }
+
+    /**
+     * @param fname     File name
+     * @param formatos  Accepted formats
+     * @return          result of validation
+     */
+    public static boolean validateFile(String fname, String... formatos){
+        fname = fname.toLowerCase();
+        String extension = fname.substring(fname.length()-4);
+
+        for (String ext: formatos) {
+            if (extension.contains(ext)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    private static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    private static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    private static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
     }
 }

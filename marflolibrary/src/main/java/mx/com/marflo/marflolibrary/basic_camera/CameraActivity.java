1d2b447b9ff093721412a386bee5df7b749eea27
AlexMarFlo <ing.amarflo@gmail.com>
2018-09-20 12:09:14 -0500

    implement CameraActivity

---
+++ marflolibrary/src/main/java/mx/com/marflo/marflolibrary/basic_camera/CameraActivity.java
@@ -0,0 +1,54 @@
+package mx.com.marflo.marflolibrary.basic_camera;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+import mx.com.marflo.marflolibrary.PersonalDialog;
+import mx.com.marflo.marflolibrary.R;
+
+public class CameraActivity extends AppCompatActivity {
+
+    public static final String DESTINY  = "destiny";
+    public static final String RESULT   = "result";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        if (getIntent().getExtras() == null){
+            new PersonalDialog().showDialog(
+                    this,
+                    "Sin data",
+                    "No se recibió el objeto File de destino para la imágen",
+                    PersonalDialog.ICON.WARNING, new PersonalDialog.Callback() {
+                        @Override
+                        public void onPositiveClick(AlertDialog alertDialog) {
+                            alertDialog.dismiss();
+                            finish();
+                        }
+
+                        @Override
+                        public void onNeutralClick(AlertDialog alertDialog) {
+
+                        }
+
+                        @Override
+                        public void onNegativeClick(AlertDialog alertDialog) {
+
+                        }
+                    });
+            return;
+        }
+
+        if(savedInstanceState == null){
+            Fragment fr = CameraBasicFragment.newInstance();
+            fr.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, fr)
+                    .commit();
+        }
+    }
+}
--- marflolibrary/src/main/java/mx/com/marflo/marflolibrary/getCamera.java
+++ marflolibrary/src/main/java/mx/com/marflo/marflolibrary/getCamera.java
@@ -3,12 +3,11 @@ package mx.com.marflo.marflolibrary;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.provider.MediaStore;
-import android.widget.Toast;
 
 import java.io.File;
 
+import mx.com.marflo.marflolibrary.basic_camera.CameraActivity;
 import mx.com.marflo.marflolibrary.permisos_android.AndroidRuntimePermits;
 import mx.com.marflo.marflolibrary.permisos_android.androidPermits;
 
@@ -47,25 +46,19 @@ public class getCamera {
         if (androidPermits.verificarPermiso(context, AndroidRuntimePermits.CAMERA)){
             if (androidPermits.verificarPermiso(context, AndroidRuntimePermits.WRITE_EXTERNAL_STORAGE)){
 
+                File f = getOutputMediaFile(context, photoName);
+
                 Intent data = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                Uri fileUri = FilesUtils.getUriFromProvider(context, getOutputMediaFile(context, photoName), provider);
-                data.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                data.putExtra(MediaStore.EXTRA_OUTPUT, FilesUtils.getUriFromProvider(context, f, provider));
                 data.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                if (data.resolveActivity(context.getPackageManager()) != null) {
-                    Activity ac = (Activity) context;
-                    ac.startActivityForResult(data, requestCode);
-                }else{
-                    /*new PersonalDialog()
-                    .showDialog(
-                            context,
-                            context.getResources().getString(R.string.tomar_foto_title_no_app),
-                            context.getResources().getString(R.string.tomar_foto_message_no_app),
-                            PersonalDialog.ICON.WARNING,
-                            null);*/
-                    Toast.makeText(context,
-                            context.getResources().getString(R.string.tomar_foto_message_no_app),
-                            Toast.LENGTH_LONG).show();
+
+                if (data.resolveActivity(context.getPackageManager()) == null) {
+                    data = new Intent(context, CameraActivity.class);
+                    data.putExtra(CameraActivity.DESTINY, f);
                 }
+
+                Activity ac = (Activity) context;
+                ac.startActivityForResult(data, requestCode);
             }
         }
     }
